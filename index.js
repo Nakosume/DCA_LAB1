5aba68d86422dab86e099832dda03215671804d3
Sebastian Romero <nakosume@gmail.com>
2023-02-11 18:33:05 -0500

    Added index.js and NPM info

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 console.log("Hello, World")
 const ng = require('name-generate');
-const cow = require('cowsayjs');
+const cs = require("cowsayjs");
 
 let name=ng.first();
 let last=ng.last();
 let fn=name+" "+last;
 
-let cs = require("cowsayjs");
+
 
 console.log(cs.moo(`mi nombre es ${fn}`,{
     cow:"dragon"
